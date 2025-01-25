ba80e802cdbaf3a9e182e032c1ca3abc4c53ce78
ErrkkSkyseer <lolplayer78876@gmail.com>
2025-01-26 01:41:43 +0700

    battle mode commit

--- Assets/Scripts/Player/Controller/BattleModeController.cs
+++ Assets/Scripts/Player/Controller/BattleModeController.cs
@@ -7,23 +7,44 @@ public enum ControllerMode
 
 public class BattleModeController : MonoBehaviour
 {
-    public ControllerMode Mode;
+    public ControllerMode Mode = ControllerMode.Topdown;
     public float m_topdownSpeed = 5.0f;
     public float m_sideScrollSpeed = 5.0f;
     public float m_sideJumpHeight = 15.0f;
 
+    [SerializeField] float localGravity = 3;
+
     private Rigidbody2D rb;
-    public Vector2 boxSize;
     public float castDistance;
     public LayerMask groundlayer;
+
+    bool isControllerActive = false;
+
+    void ActivateController() => isControllerActive = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        BattleModeManager.Instance.OnBattleModeEnter += ActivateController;
+
+        rb.gravityScale = 0;
     }
 
-    void FixedUpdate()
+    private void OnDisable()
     {
+        BattleModeManager.Instance.OnBattleModeEnter -= ActivateController;
+    }
+
+    void Update()
+    {
+        if (!isControllerActive)
+            return;
+
+        if (DialogueManager.Instance.DialogueIsPlaying)
+            return;
+
+
         Move();
     }
     public void Move()
@@ -46,19 +67,23 @@ public class BattleModeController : MonoBehaviour
     }
     public void HandleSideScroll()
     {
+        rb.gravityScale = localGravity;
+
         float m_Input = InputManager.Instance.GetMoveVector().x;
         rb.linearVelocity = new Vector2(m_Input * m_sideScrollSpeed, rb.linearVelocity.y);
     }
 
     public void HandleTopdown()
     {
+        rb.gravityScale = 0;
+
         Vector2 moveDirection = InputManager.Instance.GetMoveVector();
         rb.linearVelocity = moveDirection * m_topdownSpeed;
     }
 
     public void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        if (InputManager.Instance.IsJumpKeyDown && isGrounded())
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, m_sideJumpHeight);
     }
     public void Ability()
@@ -67,7 +92,7 @@ public class BattleModeController : MonoBehaviour
     }
     public bool isGrounded()
     {
-        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundlayer))
+        if (Physics2D.Raycast(transform.position, Vector2.down, castDistance,groundlayer))
         {
             Debug.Log("is grounded");
             return true; 
@@ -81,7 +106,7 @@ public class BattleModeController : MonoBehaviour
     }
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+        Gizmos.DrawLine(transform.position, transform.position - transform.up * castDistance);
     }
 }
 
