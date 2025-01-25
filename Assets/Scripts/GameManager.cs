c480ec992f9c35dbb6fab04091bf6f3426a05678
ErrkkSkyseer <lolplayer78876@gmail.com>
2025-01-25 19:14:30 +0700

    ProtoScene Commit 4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
 
 public enum CameraMode
 {
     Static,
+    FollowRight,
     ClampDynamic
 }
 
@@ -13,8 +16,25 @@ public class GameManager : MonoBehaviour
     Fade fade;
 
     [Header("Game")]
+    [SerializeField] Transform player;
+
     [SerializeField] int teleportBackTimes = 3;
     int teleportBackCount = 0;
+
+    [Header("Camera Handle")]
+    CameraMode cameraMode = CameraMode.Static;
+
+    [SerializeField] Transform cameraHolder;
+    [SerializeField] MinMaxNum<float> clampDynamicRegion;
+
+    [Header("Vignette Handle")]
+    [SerializeField] PostProcessVolume postProcessingVolume;
+
+    [Range(0.0f, 1.0f)]
+    [SerializeField] float intensity;
+
+    Vignette vignette;
+
 
     public static GameManager Instance { get => instance; }
     public int TeleportBackCount { get => teleportBackCount; set => teleportBackCount = value; }
@@ -36,12 +56,30 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        fade = FindAnyObjectByType<Fade>();
+
     }
 
     private void Update()
     {
-        
+        HandleCameraState();
+    }
+
+    void HandleCameraState()
+    {
+        if (IsInStartLoop()) return;
+
+        if (player.position.x > cameraHolder.position.x && player.position.x < clampDynamicRegion.min)
+            cameraMode = CameraMode.FollowRight;
+        else if (player.position.x > clampDynamicRegion.min && player.position.x < clampDynamicRegion.max)
+            cameraMode = CameraMode.ClampDynamic;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector2 init = Vector2.right * clampDynamicRegion.min + Vector2.up * -10;
+        Vector2 end = Vector2.right * clampDynamicRegion.max + Vector2.up * -10;
+        Gizmos.DrawLine(init, end);
     }
 
 }
