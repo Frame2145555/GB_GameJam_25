0f6afc9ce4df35456749a1c21d5cedc4de76ec3c
Frame2145555 <155990240+Frame2145555@users.noreply.github.com>
2025-01-26 11:49:18 +0700

    GameplayCutscene

--- Assets/Animation/characteranim/yay/grannyanimation.cs
+++ Assets/Animation/characteranim/yay/grannyanimation.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class grannyanimation : celineanimation
 {
     int count=0;
-public void loopcount(){count++;}
+    public void loopcount(){count++;}
     public void resetcount(){count=0;}
 
     void Update()
---
+++ Assets/Scenes/Frame/Code_Frame/ShowImageBoy.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShowImageBoy : MonoBehaviour
+{
+    [Header("UI Settings")]
+    [SerializeField] private GameObject imageBoy;
+
+    private bool isPlayerInside = false;
+
+    private void Start()
+    {
+        if (imageBoy != null)
+        {
+            imageBoy.SetActive(false);
+        }
+        else
+        {
+            Debug.LogWarning("ImageBoy is not assigned in the inspector!");
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInside && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (imageBoy != null)
+            {
+                imageBoy.SetActive(false);
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInside = true;
+
+            if (imageBoy != null)
+            {
+                imageBoy.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInside = false;
+
+            if (imageBoy != null)
+            {
+                imageBoy.SetActive(false);
+            }
+        }
+    }
+}
